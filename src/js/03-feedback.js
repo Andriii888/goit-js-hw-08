839392612495c06d4a4381763c9b8ac3486fd275
Andriii888 <andrii888@gmail.com>
2022-11-21 01:02:56 +0300

    1taskof3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
+const inputRef = document.querySelector('.feedback-form');
 
+addEventListener('input', onWritingValue);
+const obj = {};
+function onWritingValue(e) {
+
+   if (e.target.name === 'email') {
+   obj.email = e.target.value;
+    };
+    if (e.target.name === 'message') {
+        obj.message = e.target.value;
+    }
+   
+    const storageValue = JSON.stringify(obj);
+   
+    return localStorage.setItem('feedback-form-state', storageValue );
+     };
