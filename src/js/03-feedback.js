b31320069a1139bea9428afa2ab5b0931d7801c7
Andriii888 <andrii888@gmail.com>
2022-11-21 02:25:45 +0300

    add32

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ const inputRef = document.querySelector('.feedback-form');
 addEventListener('input', onWritingValue);
 const obj = {};
 function onWritingValue(e) {
-
+   
    if (e.target.name === 'email') {
    obj.email = e.target.value;
     };
@@ -14,4 +14,17 @@ function onWritingValue(e) {
     const storageValue = JSON.stringify(obj);
    
     return localStorage.setItem('feedback-form-state', storageValue );
-     };
+};
+
+const inputEmail = inputRef.children[0];
+const inputMessage = inputRef.children[1];
+function listenerForStorage() {
+    const getValue = localStorage.getItem('feedback-form-state');
+    if (getValue === null) {
+        return console.log(getValue) ;
+    }
+    const itValue = JSON.parse(getValue);
+    inputEmail.firstElementChild.textContent = itValue.email;
+    inputMessage.firstElementChild.textContent = itValue.message;
+};
+listenerForStorage();
