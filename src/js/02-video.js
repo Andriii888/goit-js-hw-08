d4ad6d7cfe6a8f09145605569bf599f0dcc13a00
Andriii888 <andrii888@gmail.com>
2022-11-20 22:08:19 +0300

    add2all

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,32 @@ const iframe = document.querySelector('iframe');
     player.on('play', function() {
         console.log('played the video!');
     });
+ player.on('timeupdate', function(data) {
+    const stringCurrentTimeOfVideo =JSON.stringify(data)
+     localStorage.setItem('videoplayer-current-time', stringCurrentTimeOfVideo);
+     
+    
+ });
+
+   let getCurrentTime = localStorage.getItem('videoplayer-current-time');
+let secondsValueOfVideo = JSON.parse(getCurrentTime);
+let seconds = secondsValueOfVideo.seconds;
+
+player.setCurrentTime(seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
 
     player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
+    
