566c97e575556870f9f91c55bd3aa3af8716d50b
Andriii888 <andrii888@gmail.com>
2022-11-20 22:25:34 +0300

    add2throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,19 @@
 import playerForV from '@vimeo/player';
+let throttleForVideo = require('lodash.throttle');
+
 const iframe = document.querySelector('iframe');
     const player = new playerForV(iframe);
 
     player.on('play', function() {
         console.log('played the video!');
     });
- player.on('timeupdate', function(data) {
+
+ player.on('timeupdate', throttleForVideo((data) => {
     const stringCurrentTimeOfVideo =JSON.stringify(data)
      localStorage.setItem('videoplayer-current-time', stringCurrentTimeOfVideo);
      
     
- });
+ },1000));
 
    let getCurrentTime = localStorage.getItem('videoplayer-current-time');
 let secondsValueOfVideo = JSON.parse(getCurrentTime);
